94f7743c5abd78f33eee129e69400f220a88a756
KaranSherathiya <109953391+KaranSherathiya@users.noreply.github.com>
2025-01-27 02:56:46 +0530

    Add files via upload

---
+++ src/commands/about.ts
@@ -0,0 +1,52 @@
+import command from '../../config.json' assert {type: 'json'};
+
+const createAbout = () : string[] => {
+  const about : string[] = [];
+
+  const SPACE = "&nbsp;";
+
+  const EMAIL = "Email";
+  const GITHUB = "Github";
+  const LINKEDIN = "Linkedin";
+  
+  const email = `<i class='fa-solid fa-envelope'></i> ${EMAIL}`;   
+  const github = `<i class='fa-brands fa-github'></i> ${GITHUB}`;
+  const linkedin = `<i class='fa-brands fa-linkedin'></i> ${LINKEDIN}`;
+  let string = "";
+
+  about.push("<br>");
+  about.push(command.aboutGreeting);
+  about.push("<br>");
+  string += SPACE.repeat(2);
+  string += email;
+  string += SPACE.repeat(17 - EMAIL.length);
+  string += `<a target='_blank' href='mailto:${command.social.email}'>${command.social.email}</a>`;
+  about.push(string);
+
+  string = '';
+  string += SPACE.repeat(2);
+  string += github;
+  string += SPACE.repeat(17 - GITHUB.length);
+  string += `<a target='_blank' href='https://github.com/${command.social.github}'>github/${command.social.github}</a>`;
+  about.push(string);
+
+  string = '';
+  string += SPACE.repeat(2);
+  string += linkedin;
+  string += SPACE.repeat(17 - LINKEDIN.length);  
+  string += `<a target='_blank' href='https://www.linkedin.com/in/${command.social.linkedin}'>linkedin/${command.social.linkedin}</a>`;
+  about.push(string);
+  
+  //resume section link
+  string = '';
+  string += SPACE.repeat(2);
+  string += `<i class='fa fa-file'></i> Resume`; 
+  string += SPACE.repeat(17 - 'LINKED'.length);  
+  string += `<a target='_blank' href='https://drive.googlecom/file/d/1sB7nLodXzJM0q2Ag2SyCW7G5rfi0CR6y/view?usp=sharing'>link</a>`;
+  about.push(string);
+
+  about.push("<br>");
+  return about
+}
+
+export const ABOUT = createAbout();
---
+++ src/commands/whoami.ts
@@ -0,0 +1,31 @@
+const whoamiObj = {
+  "message": [
+    [
+      "I am Karan Sherathiya,",
+      "I like working on NLP and ML/Deep Learning models",
+      "Can talk for hours about Global market and working of Finance world ",
+      "",
+      "Currently researching about Blockchain ",
+      "",
+    ]
+  ]
+}
+
+
+
+export const createWhoami = () : string[] => {
+  const whoami : string[] = [];  
+  const r = Math.floor(Math.random() * whoamiObj.message.length);
+  whoami.push("<br>");
+
+  whoamiObj.message[r].forEach((ele, idx) => {
+    if (idx === whoamiObj.message[r].length - 1) {
+      ele += "<span class='command'></span>";
+    }
+    whoami.push(ele);
+  });
+
+  whoami.push("<br>");
+
+  return whoami
+}
